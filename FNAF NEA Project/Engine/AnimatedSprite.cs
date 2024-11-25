9d0aa516dcccebc8acdb70bd653c50242946186a
Kepler <harveyhellewell10@gmail.com>
2024-11-25 18:59:38 +0000

    Started work on animated sprites

---
+++ FNAF NEA Project/Engine/AnimatedSprite.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using NEA_Project.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+# nullable enable
+
+namespace FNAF_NEA_Project.Engine
+{
+    public class AnimatedSprite : DrawItem
+    {
+        protected string textureName = "";
+        protected Texture2D? texture = null;
+        public Texture2D? Texture { get { return texture; } }
+
+        public AnimatedSprite() { }
+        public AnimatedSprite(string textureName)
+        {
+            this.textureName = textureName;
+            LoadContent();
+        }
+
+
+        // What to load
+        public override void LoadContent()
+        {
+            TextureManager.AddTexture(textureName);
+            texture = TextureManager.GetTexture(textureName);
+        }
+
+        // What to draw
+        public override void Draw()
+        {
+            if (texture != null && Visible)
+            {
+                Vector2 scale = GlobalCamera.ApplyCameraScale(dp.Scale);
+                Vector2 pos = GlobalCamera.ApplyCameraPosition(dp.Pos);
+                float rot = GlobalCamera.ApplyCameraRotation(dp.Rot);
+                Color colour = GlobalCamera.ApplyCameraColour(dp.Colour);
+                MonogameGraphics._spriteBatch.Draw(texture, pos, dp.Size, colour, rot, dp.Origin, scale, dp.Effects, 1.0f);
+            }
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+    }
+}
---
+++ FNAF NEA Project/Engine/AnimationData.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using NEA_Project.Engine;
+using SharpDX.Direct3D9;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#nullable enable
+
+namespace FNAF_NEA_Project.Engine
+{
+    public class AnimationData
+    {
+        protected string[] Frames;
+        protected Texture2D?[] FrameTextures;
+        public float FPS;
+        public bool DoesLoop;
+
+        public AnimationData(string[] frames)
+        {
+            Frames = frames;
+            FPS = 24;
+            DoesLoop = false;
+            SetFrameTextures();
+        }
+
+        public AnimationData(string[] frames, bool doesLoop)
+        {
+            Frames = frames;
+            FPS = 24;
+            DoesLoop = doesLoop;
+            SetFrameTextures();
+        }
+
+        public AnimationData(string[] frames, float fps)
+        {
+            Frames = frames;
+            FPS = fps;
+            DoesLoop = false;
+            SetFrameTextures();
+        }
+
+        public AnimationData(string[] frames, float fPS, bool doesLoop)
+        {
+            Frames = frames;
+            FPS = fPS;
+            DoesLoop = doesLoop;
+            SetFrameTextures();
+        }
+
+        protected void SetFrameTextures()
+        {
+            if (Frames != null)
+            {
+                if (FrameTextures == null) FrameTextures = new Texture2D?[Frames.Length];
+                for (int i = 0; i < Frames.Length; i++)
+                {
+                    TextureManager.AddTexture(Frames[i]);
+                    FrameTextures[i] = TextureManager.GetTexture(Frames[i]);
+                }
+            }
+        }
+    }
+}
